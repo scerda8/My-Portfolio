fc4c56b4ac2d069c79afa9e74c8bfcb148d81aa5
scerda8 <shandycerda@shandys-mbp-2.mynetworksettings.com>
2024-07-22 17:07:18 -0400

    test

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -4,11 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './', 
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000,
+  css:{
+    modules:{
+      localsConvention:"camelCase",
+    },
   },
 });
